3d842bba538bbc95366d75d018397c992830bda3
PauCaMu <pau.casas@institutvidreres.cat>
2023-06-02 17:17:01 +0200

    tot fet

--- .idea/nodeJS.js
+++ .idea/nodeJS.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const fs1 = require('node:fs')
-const stream = require('stream')
-const mysql = require('mysql2')
-const Sequelize = require('sequelize')
+const mysql = require('mysql2');
+const { getFirestore } = require('firebase-admin/firestore');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+const axios = require('axios');
 
 
-//prova
 app.use(cors());
 app.use(express.json());
-port = 3080;
+const port = 3080;
 
 let connexio_BD;
 let conexioMySQL;
@@ -33,8 +34,8 @@ const connexioMySQL = mysql.createConnection({
     user : configObj.username,
     password : configObj.password,
     host : configObj.host,
-
 });
+
 
 connexioMySQL.connect((err) => {
     if (err) throw err;
@@ -42,88 +43,195 @@ connexioMySQL.connect((err) => {
 });
 
 
+setTimeout(function () {
+    connexio();
+}, 2000);
 
-
-const {getFirestore} = require("firebase-admin/firestore");
-var admin = require("firebase-admin");
-
-setTimeout(function (){connexio()}, 2000);
-
-function connexio(){
+function connexio() {
     var serviceAccount = require(connexio_BD);
 
     if (admin.apps.length === 0) {
         admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount)
+            credential: admin.credential.cert(serviceAccount),
         });
     }
 }
 
+app.put('/registre', cors(), async (req, res) => {
+    const db = getFirestore();
+    const usuari = {
+        nom: req.body.nom,
+        cognom: req.body.cognom,
+        telefon: req.body.telefon,
+        email: req.body.email,
+        passw: req.body.passw,
+    };
 
+    const cityRef = db.collection('ProjecteLEA').doc(req.body.email);
+    const doc = await cityRef.get();
 
+    if (doc.exists) {
+        console.log('Usuari ja existeix');
+    } else {
+        await db.collection('ProjecteLEA').doc(req.body.email).set(usuari);
+        console.log(usuari);
+    }
+});
 
+app.get('/email', cors(), async (req, res) => {
+    const db = getFirestore();
+    let correu = { email: req.query.email };
+    console.log(correu);
+    let resultat = false;
+    const docs = db.collection('ProjecteLEA');
+    const snapshot = await docs.where('email', '==', correu.email).get();
+    snapshot.forEach((doc) => {
+        resultat = true;
+    });
+    res.json(resultat);
+});
 
-    app.put('/registre', cors(), async (req, res) => {
-        const db = getFirestore();
-        const usuari = {
-            'nom': req.body.nom,
-            'cognom': req.body.cognom,
-            'telefon': req.body.telefon,
-            'email': req.body.email,
-            'passw': req.body.passw
-        };
+app.get('/pass', cors(), async (req, res) => {
+    const db = getFirestore();
+    let correu = { passw: req.query.passw };
+    let resultat = false;
+    const docs = db.collection('ProjecteLEA');
+    const snapshot = await docs.where('passw', '==', correu.passw).get();
+    snapshot.forEach((doc) => {
+        resultat = true;
+    });
+    res.json(resultat);
+});
 
-        const cityRef = db.collection('ProjecteLEA').doc(req.body.email);
-        const doc = await cityRef.get();
+app.get('/usuarios', cors(), async (req, res) => {
+    const db = getFirestore();
+    const snapshot = await db.collection('ProjecteLEA').get();
+    const usuarios = snapshot.docs.map((doc) => doc.data());
+    res.json(usuarios);
+});
 
-        if (doc.exists) {
-            console.log('Usuari ja existeix');
-        } else {
-            await db.collection('ProjecteLEA').doc(req.body.email).set(usuari);
-            console.log(usuari)
-        }
+async function sendEmail(name, email) {
+    const data = JSON.stringify({
+        Messages: [
+            {
+                From: { Email: 'pau.casas@institutvidreres.cat', Name: 'Pau' },
+                To: [{ Email: email, Name: name }],
+                Subject: 'Cambiar tu contraseña',
+                TextPart:
+                    'Hola buenas, \n' +
+                    'Hemos recibido una solicitud de cambio de contraseña, en caso de ser tú quien ha solicitado esto, entra en el enlace que tienes a continuación. En caso contrario, alguien está intentando acceder a tu cuenta. \n' +
+                    '\n' +
+                    'http://localhost:4200/cambiar-contrasenya \n\n' +
+                    '\n' +
+                    'Atentamente: Pau Casas \n\n' +
+                    '\n' +
+                    'Tu Aplicación.',
+            },
+        ],
     });
 
+    const config = {
+        method: 'post',
+        url: 'https://api.mailjet.com/v3.1/send',
+        data: data,
+        headers: { 'Content-Type': 'application/json' },
+        auth: {
+            username: '0909b31b6321a4f6a96c1ffaf6cff0bf',
+            password: '6a777ad8f6123738fab170072ad26dff',
+        },
+    };
 
-// Comprovar si existeix l'email
-    app.get('/email', cors(), async (req, res) => {
-        const db = getFirestore();
-        let correu = {email: req.query.email}
-        console.log(correu)
-        let resultat = false;
-        const docs = db.collection('ProjecteLEA')
-        const snapshot = await docs.where('email', '==', correu.email).get()
-        snapshot.forEach(doc => {
-            resultat = true
+    return axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
         })
-        res.json(resultat)
-    });
+        .catch(function (error) {
+            console.log(error);
+        });
+}
 
-// Comprovar la contrasenya
-    app.get('/pass', cors(), async (req, res) => {
-        const db = getFirestore();
-        let correu = {passw: req.query.passw}
-        let resultat = false;
-        const docs = db.collection('ProjecteLEA')
-        const snapshot = await docs.where('passw', '==', correu.passw).get()
-        snapshot.forEach(doc => {
-            resultat = true
+app.post('/api/sendemail', function (req, res) {
+    console.log('Correo Enviado');
+    const { name, email } = req.body;
+    sendEmail(name, email)
+        .then(() => {
+            res.sendStatus(200);
         })
-        res.json(resultat)
+        .catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+});
+
+app.post('/api/contrasenya', async (req, res) => {
+    const { email, contra } = req.body;
+    let documento = '';
+    const db = getFirestore();
+    const docs = db.collection('ProjecteLEA');
+    const snapshot = await docs.where('email', '==', email).get();
+    snapshot.forEach((doc) => {
+        documento = doc.id;
     });
+    const modify = await db
+        .collection('ProjecteLEA')
+        .doc(documento)
+        .set({ passw: contra }, { merge: true });
+    res.json(contra);
+});
+
+app.get('/api/nombre', async (req, res) => {
+    const email = req.query.email;
+    let documento = '';
+    const db = getFirestore();
+    const docs = db.collection('ProjecteLEA');
+    const snapshot = await docs.where('email', '==', email).get();
+    snapshot.forEach((doc) => {
+        documento = doc.data();
+    });
+    res.json(documento);
+});
+
+app.get('/usuario/:email', cors(), async (req, res) => {
+    const db = getFirestore();
+    const email = req.params.email;
+    const doc = await db.collection('ProjecteLEA').doc(email).get();
+    if (doc.exists) {
+        const usuario = doc.data();
+        res.json(usuario);
+    } else {
+        res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+});
+
+app.put('/usuarios/:email', cors(), async (req, res) => {
+    const db = getFirestore();
+    const email = req.params.email;
+    const data = req.body;
+
+    try {
+        await db.collection('ProjecteLEA').doc(email).update(data);
+        res.sendStatus(200);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+});
+
+
+
 
 
 //Servidor escolta pel port
-    app.listen(port, () => {
-        console.log('Server listening on the port: ' + port);
-    });
+app.listen(port, () => {
+    console.log('Server listening on the port: ' + port);
+});
 
 
 //logs del fitxer
- app.post('/log/pro1',(req , res) =>{
-     fs1.writeFileSync("logs",req.body.text,{flag:'a+'})
-     console.log("Producte 1 afegit")
- });
+app.post('/log/pro1',(req , res) =>{
+    fs1.writeFileSync("logs",req.body.text,{flag:'a+'})
+    console.log("Producte 1 afegit")
+});
 
 app.post('/log/pro2',(req , res) =>{
     fs1.writeFileSync("logs",req.body.text,{flag:'a+'})
